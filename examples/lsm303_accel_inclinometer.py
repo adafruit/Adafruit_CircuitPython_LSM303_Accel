659a8e379e84b501acf12cda3d575f3cd66d97e5
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:25 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/lsm303_accel_inclinometer.py
+++ examples/lsm303_accel_inclinometer.py
@@ -10,6 +10,7 @@ import adafruit_lsm303_accel
 
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_lsm303_accel.LSM303_Accel(i2c)
 
 
